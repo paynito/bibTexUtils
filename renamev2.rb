4533822f6652fde87013ea5513b4b08d15e139d3
paynito <paynito@users.noreply.github.com>
2016-06-05 18:10:09 +0300

    Add files via upload

---
+++ renamev2.rb
@@ -0,0 +1,11 @@
+#!/Users/AbuDavid/.rvm/rubies/ruby-head/bin/ruby
+array = ['cat','dog','bird']
+array.push 'cat'
+puts array
+puts array.index('dog')
+#array.each { |x|
+ # counter = 1
+
+#puts x
+
+#}
